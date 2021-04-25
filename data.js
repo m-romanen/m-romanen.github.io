58c628357f88d204aa1f1380599e088d4a3bb844
M.Romanenko <biz.mikhailr@gmail.com>
2021-04-25 05:18:42 +0300

    refactorig, add rules, add statistics

--- data.js
+++ data.js
@@ -14,212 +14,95 @@ document.addEventListener('DOMContentLoaded', function() {
     NOVEMBER = 10;
     DECEMBER = 11;
 
-    FIRST_PLACE = 5;
-    SECOND_PLACE = 4;
-    THIRD_PLACE = 3;
-    PARTICIPANT = 1;
-    NOT_PARTICIPANT = 0;
-
     data = {};
 
     data.players = [
-        { id: 1, name: 'Миша gr1zzly', rating: 0 },
-        { id: 2, name: 'Лера', rating: 0 },
-        { id: 3, name: 'Миша', rating: 0 },
-        { id: 4, name: 'Юля', rating: 0 },
-        { id: 5, name: 'Повар', rating: 0 },
-        { id: 6, name: 'Паша', rating: 0 },
-        { id: 7, name: 'Миша Андреевич', rating: 0 },
-        { id: 8, name: 'Вдович', rating: 0 },
-        { id: 9, name: 'Жека', rating: 0 },
-        { id: 10, name: 'Кирилл', rating: 0 },
-        { id: 11, name: 'Коча', rating: 0 },
-        { id: 12, name: 'Гасан', rating: 0 }
+        { id: 1, name: 'Миша gr1zzly' },
+        { id: 2, name: 'Лера' },
+        { id: 3, name: 'Миша' },
+        { id: 4, name: 'Юля' },
+        { id: 5, name: 'Повар' },
+        { id: 6, name: 'Паша' },
+        { id: 7, name: 'Миша Андреевич' },
+        { id: 8, name: 'Вдович' },
+        { id: 9, name: 'Жека' },
+        { id: 10, name: 'Кирилл' },
+        { id: 11, name: 'Коча' },
+        { id: 12, name: 'Гасан' }
     ];
 
+    data.games = [
+        { date: new Date(2021, APRIL, 25), buyIn: 100, results: [1, 8, 7, 2, 3, 4, 5, 6, 9] },
+        { date: new Date(2021, APRIL, 11), buyIn: 50, results: [6, 4, 7, 2, 3, 5, 9, 10, 11, 12] },
+        { date: new Date(2021, APRIL, 4), buyIn: 50, results: [3, 2, 1, 4, 5, 6, 8, 9, 10, 11] },
+        { date: new Date(2021, MARCH, 21), buyIn: 50, results: [7, 4, 8, 1, 2, 3, 5, 6, 9, 10] },
+        { date: new Date(2021, MARCH, 13), buyIn: 50, results: [1, 10, 6, 2, 3, 4, 5, 7, 8, 9] },
+        { date: new Date(2021, MARCH, 4), buyIn: 50, results: [1, 7, 3, 2, 4, 5, 6, 10] },
+        { date: new Date(2021, FEBRUARY, 28), buyIn: 0, results: [9, 5, 3, 4, 6, 11] },
+        { date: new Date(2021, FEBRUARY, 6), buyIn: 0, results: [5, 9, 6, 3, 4, 10] },
+        { date: new Date(2021, JANUARY, 24), buyIn: 0, results: [10, 1, 2, 3, 4, 5, 6, 8, 9] },
+        { date: new Date(2021, JANUARY, 9), buyIn: 0, results: [1, 2, 3, 4, 5, 6, 9, 10, 11] }
+    ];
 
-
-    data.games = [{
-        date: new Date(2021, APRIL, 25),
-        buyIn: 100,
-        results: [
-            { player: 1, score: FIRST_PLACE },
-            { player: 2, score: PARTICIPANT },
-            { player: 3, score: PARTICIPANT },
-            { player: 4, score: PARTICIPANT },
-            { player: 5, score: PARTICIPANT },
-            { player: 6, score: PARTICIPANT },
-            { player: 7, score: THIRD_PLACE },
-            { player: 8, score: SECOND_PLACE },
-            { player: 9, score: PARTICIPANT },
-            { player: 10, score: NOT_PARTICIPANT },
-            { player: 11, score: NOT_PARTICIPANT },
-            { player: 12, score: NOT_PARTICIPANT }
-        ]
-    }, {
-        date: new Date(2021, APRIL, 11),
-        buyIn: 50,
-        results: [
-            { player: 1, score: NOT_PARTICIPANT },
-            { player: 2, score: PARTICIPANT },
-            { player: 3, score: PARTICIPANT },
-            { player: 4, score: SECOND_PLACE },
-            { player: 5, score: PARTICIPANT },
-            { player: 6, score: FIRST_PLACE },
-            { player: 7, score: THIRD_PLACE },
-            { player: 8, score: NOT_PARTICIPANT },
-            { player: 9, score: PARTICIPANT },
-            { player: 10, score: PARTICIPANT },
-            { player: 11, score: PARTICIPANT },
-            { player: 12, score: PARTICIPANT }
-        ]
-    }, {
-        date: new Date(2021, APRIL, 4),
-        buyIn: 50,
-        results: [
-            { player: 1, score: THIRD_PLACE },
-            { player: 2, score: SECOND_PLACE },
-            { player: 3, score: FIRST_PLACE },
-            { player: 4, score: PARTICIPANT },
-            { player: 5, score: PARTICIPANT },
-            { player: 6, score: PARTICIPANT },
-            { player: 7, score: NOT_PARTICIPANT },
-            { player: 8, score: PARTICIPANT },
-            { player: 9, score: PARTICIPANT },
-            { player: 10, score: PARTICIPANT },
-            { player: 11, score: PARTICIPANT }
-        ]
-    }, {
-        date: new Date(2021, MARCH, 21),
-        buyIn: 50,
-        results: [
-            { player: 1, score: PARTICIPANT },
-            { player: 2, score: PARTICIPANT },
-            { player: 3, score: PARTICIPANT },
-            { player: 4, score: SECOND_PLACE },
-            { player: 5, score: PARTICIPANT },
-            { player: 6, score: PARTICIPANT },
-            { player: 7, score: FIRST_PLACE },
-            { player: 8, score: THIRD_PLACE },
-            { player: 9, score: PARTICIPANT },
-            { player: 10, score: PARTICIPANT },
-            { player: 11, score: NOT_PARTICIPANT }
-        ]
-    }, {
-        date: new Date(2021, MARCH, 13),
-        buyIn: 50,
-        results: [
-            { player: 1, score: FIRST_PLACE },
-            { player: 2, score: PARTICIPANT },
-            { player: 3, score: PARTICIPANT },
-            { player: 4, score: PARTICIPANT },
-            { player: 5, score: PARTICIPANT },
-            { player: 6, score: THIRD_PLACE },
-            { player: 7, score: PARTICIPANT },
-            { player: 8, score: PARTICIPANT },
-            { player: 9, score: PARTICIPANT },
-            { player: 10, score: SECOND_PLACE },
-            { player: 11, score: NOT_PARTICIPANT }
-        ]
-    }, {
-        date: new Date(2021, MARCH, 4),
-        buyIn: 50,
-        results: [
-            { player: 1, score: FIRST_PLACE },
-            { player: 2, score: PARTICIPANT },
-            { player: 3, score: THIRD_PLACE },
-            { player: 4, score: PARTICIPANT },
-            { player: 5, score: PARTICIPANT },
-            { player: 6, score: PARTICIPANT },
-            { player: 7, score: SECOND_PLACE },
-            { player: 8, score: NOT_PARTICIPANT },
-            { player: 9, score: NOT_PARTICIPANT },
-            { player: 10, score: PARTICIPANT },
-            { player: 11, score: NOT_PARTICIPANT }
-        ]
-    }, {
-        date: new Date(2021, FEBRUARY, 28),
-        buyIn: 0,
-        results: [
-            { player: 1, score: NOT_PARTICIPANT },
-            { player: 2, score: NOT_PARTICIPANT },
-            { player: 3, score: THIRD_PLACE },
-            { player: 4, score: PARTICIPANT },
-            { player: 5, score: SECOND_PLACE },
-            { player: 6, score: PARTICIPANT },
-            { player: 7, score: NOT_PARTICIPANT },
-            { player: 8, score: NOT_PARTICIPANT },
-            { player: 9, score: FIRST_PLACE },
-            { player: 10, score: NOT_PARTICIPANT },
-            { player: 11, score: PARTICIPANT }
-        ]
-    }, {
-        date: new Date(2021, FEBRUARY, 6),
-        buyIn: 0,
-        results: [
-            { player: 1, score: NOT_PARTICIPANT },
-            { player: 2, score: NOT_PARTICIPANT },
-            { player: 3, score: PARTICIPANT },
-            { player: 4, score: PARTICIPANT },
-            { player: 5, score: FIRST_PLACE },
-            { player: 6, score: THIRD_PLACE },
-            { player: 7, score: NOT_PARTICIPANT },
-            { player: 8, score: NOT_PARTICIPANT },
-            { player: 9, score: SECOND_PLACE },
-            { player: 10, score: PARTICIPANT },
-            { player: 11, score: NOT_PARTICIPANT }
-        ]
-    }, {
-        date: new Date(2021, JANUARY, 24),
-        buyIn: 0,
-        results: [
-            { player: 1, score: SECOND_PLACE },
-            { player: 2, score: THIRD_PLACE },
-            { player: 3, score: PARTICIPANT },
-            { player: 4, score: PARTICIPANT },
-            { player: 5, score: PARTICIPANT },
-            { player: 6, score: PARTICIPANT },
-            { player: 7, score: NOT_PARTICIPANT },
-            { player: 8, score: PARTICIPANT },
-            { player: 9, score: PARTICIPANT },
-            { player: 10, score: FIRST_PLACE },
-            { player: 11, score: NOT_PARTICIPANT }
-        ]
-    }, {
-        date: new Date(2021, JANUARY, 9),
-        buyIn: 0,
-        results: [
-            { player: 1, score: FIRST_PLACE },
-            { player: 2, score: SECOND_PLACE },
-            { player: 3, score: THIRD_PLACE },
-            { player: 4, score: PARTICIPANT },
-            { player: 5, score: PARTICIPANT },
-            { player: 6, score: PARTICIPANT },
-            { player: 7, score: NOT_PARTICIPANT },
-            { player: 8, score: NOT_PARTICIPANT },
-            { player: 9, score: PARTICIPANT },
-            { player: 10, score: PARTICIPANT },
-            { player: 11, score: PARTICIPANT }
-        ]
-    }];
-
-    //calculate rating
-    data.games.forEach(game => {
-        game.results.forEach(result => {
-            data.players.find(player => player.id == result.player).rating += result.score
-        })
-    });
-
-    data.players.sort(function(a, b) {
-        if (a.rating > b.rating) {
-            return -1;
+    data.getPointsByPlace = function(place) {
+        if (place == 1) {
+            return 5;
         }
-        if (a.rating < b.rating) {
-            return 1;
+        if (place == 2) {
+            return 4;
         }
-        return 0;
-    });
+        if (place == 3) {
+            return 3;
+        }
+        return 1;
+    };
 
-    data.prizeMoney = 0;
-    data.games.forEach(game => data.prizeMoney += game.buyIn);
+    data.calculatePlayersData = function() {
+        data.players.forEach(player => {
+            player.rating = 0;
+            player.gamesCount = 0;
+            player.firstPlaces = 0;
+            player.secondPlaces = 0;
+            player.thirdPlaces = 0;
+        });
+        data.games.forEach(game => {
+            place = 1;
+            game.results.forEach(result => {
+                data.players.find(player => player.id == result).rating += data.getPointsByPlace(place);
+                if (place == 1) {
+                    data.players.find(player => player.id == result).firstPlaces++;
+                }
+                if (place == 2) {
+                    data.players.find(player => player.id == result).secondPlaces++;
+                }
+                if (place == 3) {
+                    data.players.find(player => player.id == result).thirdPlaces++;
+                }
+                data.players.find(player => player.id == result).gamesCount++;
+                place++;
+            })
+        });
+    };
+
+    data.sortPlayersByRating = function() {
+        data.players.sort(function(a, b) {
+            if (a.rating > b.rating) {
+                return -1;
+            }
+            if (a.rating < b.rating) {
+                return 1;
+            }
+            return 0;
+        });
+    };
+
+    data.calculatePrizePool = function() {
+        data.prizePool = 0;
+        data.games.forEach(game => data.prizePool += game.buyIn);
+    };
+
+    data.calculatePlayersData();
+    data.sortPlayersByRating();
+    data.calculatePrizePool();
+
 });
--- draw.js
+++ draw.js
@@ -1,12 +1,12 @@
 ;
 window.onload = (event) => {
 
-    var playersHtml = '<table>';
-    data.players.forEach(player => playersHtml += '<tr><td>' + player.name + '</td><td>' + player.rating + '</td></tr>');
+    var playersHtml = '<table><tr><td>Name</td><td>Rating</td><td>Games played</td><td>First/second/third places</td></tr>';
+    data.players.forEach(player => playersHtml += '<tr><td>' + player.name + '</td><td>' + player.rating + '</td><td>' + player.gamesCount + '</td><td>' + player.firstPlaces + '/' + player.secondPlaces + '/' + player.thirdPlaces + '</td></tr>');
     playersHtml += '</table>';
     document.getElementById('players').innerHTML += playersHtml;
 
-    document.getElementById('prizeMoney').innerHTML += data.prizeMoney;
+    document.getElementById('prizePool').innerHTML += data.prizePool + ' UAH';
 
     var gamesHtml = '<div class="splide__track"><ul class="splide__list">';
     var gameCount = data.games.length;
@@ -21,10 +21,13 @@ window.onload = (event) => {
             return 0;
         });
         gamesHtml += '<li class="splide__slide"><div class="game_title">Game ' + gameCount + '</div><div class="game_date">' + game.date.toLocaleDateString("ru") + '</div><table>';
-        game.results.filter(result => result.score > 0).forEach(result => {
-            gamesHtml += '<tr><td>' + data.players.find(player => player.id == result.player).name + '</td><td>+' + result.score + '</td></tr>'
+
+        var place = 1;
+        game.results.forEach(result => {
+            gamesHtml += '<tr><td>' + data.players.find(player => player.id == result).name + '</td><td>+' + data.getPointsByPlace(place) + '</td></tr>'
+            place++;
         });
-        gamesHtml += '</table><div class="buy_in">+' + game.buyIn + ' грн</div><li>';
+        gamesHtml += '</table><div class="buy_in">+' + game.buyIn + ' uah</div><li>';
         gameCount--;
     });
     gamesHtml += '<ul></div>';
@@ -33,7 +36,7 @@ window.onload = (event) => {
         new Splide('#games', {
             type: 'slide',
             perPage: 4,
-            perMove: 1,
+            perMove: 2,
             speed: 600,
         }).mount();
     }, 1);
